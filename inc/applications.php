541515261756e21578d2641011785c464f2743c4
woodwardtw <twwoodward@me.com>
2022-08-10 15:35:01 -0400

    updates for app views

--- inc/applications.php
+++ inc/applications.php
@@ -17,3 +17,47 @@ function dlinq_update_purpose(){
         echo "I lack purpose. Please give me some.";
     }
 }
+
+function dlinq_update_generic_text($field_name,$alt_message){
+    if(get_field($field_name)){
+        $field_text = get_field($field_name);
+        echo "{$field_text}";
+    } else {
+        echo $alt_message;
+    }
+}
+
+function dlinq_update_app_updates(){
+    global $post;
+    $post_slug = $post->post_name;
+    $args = array( 
+        'post_type' => 'update',
+        'posts_per_page' => 25,
+        'post_status'=>'published',
+        'tax_query' => array( // (array) - use taxonomy parameters (available with Version 3.1).
+            array(
+              'taxonomy' => 'software', // (string) - Taxonomy.
+              'field' => 'slug', // (string) - Select taxonomy term by Possible values are 'term_id', 'name', 'slug' or 'term_taxonomy_id'. Default value is 'term_id'.
+              'terms' => array( $post_slug  ), // (int/string/array) - Taxonomy term(s).              
+              'operator' => 'IN' // (string) - Operator to test. Possible values are 'IN', 'NOT IN', 'AND', 'EXISTS' and 'NOT EXISTS'. Default value is 'IN'.
+            ),
+        )
+
+    );
+
+    $the_query = new WP_Query( $args );
+    //var_dump($the_query);
+    // The Loop
+    if ( $the_query->have_posts() ) :
+    while ( $the_query->have_posts() ) : $the_query->the_post();
+      // Do Stuff
+        $title = get_the_title();
+        $link = get_the_permalink();
+        $content = get_the_content();
+        echo "<div class='update'><h3><a href='{$link}'>{$title}</a></h3><p>{$content}</p></div>";
+    endwhile;
+    endif;
+
+    // Reset Post Data
+    wp_reset_postdata();    
+}
--- loop-templates/content-single-application.php
+++ loop-templates/content-single-application.php
@@ -28,7 +28,18 @@ defined( 'ABSPATH' ) || exit;
 	<div class="entry-content">
 		<div class="purpose">
 			<h2>Purpose</h2>
-			<?php dlinq_update_purpose();?>
+			<?php dlinq_update_generic_text('what_does_it_do','Please give me purpose.');?>
+		</div>
+		<div class="users">
+			<h2>Primary Users</h2>
+			<?php dlinq_update_generic_text('primary_users','I need to be associated with someone.');?>
+		</div>
+		<div class="license">
+			<h2>License Details</h2>
+			<?php dlinq_update_generic_text('license_details','I need to be boundaries.');?>
+		</div>
+		<div class="updates">
+			<?php dlinq_update_app_updates();?>
 		</div>
 		<?php
 		// the_content();
